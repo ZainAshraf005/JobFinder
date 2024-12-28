81d5eb5be8914fb5e2a6df4a78a4171a2fe35a6a
Zain Ashraf <164740618+ZainAshraf005@users.noreply.github.com>
2024-12-28 11:27:12 +0500

    Update route.ts

--- src/app/api/job/get/[id]/route.ts
+++ src/app/api/job/get/[id]/route.ts
@@ -4,16 +4,15 @@ import { addCorsHeaders } from "@/lib/cors";
 
 export const GET = async (
   request: NextRequest,
-  context: { params: { id: string } }
+  { params }: { params: Promise<{ id: string }> }
 ) => {
   try {
-    const { id } = context.params; // Access the route parameter correctly
-    if (!id) {
+    const { id } = await params;
+    if (!id)
       return NextResponse.json(
         { message: "id is required", success: false },
         { status: 400 }
       );
-    }
 
     const userExists = await prisma.user.findUnique({
       where: { id },
@@ -25,27 +24,23 @@ export const GET = async (
       include: { Application: true },
     });
 
-    if (!userExists || userExists.role.toLowerCase() !== "employer") {
+    if (!userExists || userExists.role.toLocaleLowerCase() !== "employer")
       return NextResponse.json(
         { message: "employer not found", success: false },
         { status: 404 }
       );
-    }
 
     return NextResponse.json(
       { message: "jobs fetched", success: true, jobs: jobExists },
-      { status: 200 }
+      { status: 201 }
     );
   } catch (error: unknown) {
-    console.error(error);
-    return NextResponse.json(
-      { message: "Internal server error", success: false },
-      { status: 500 }
-    );
+    console.log(error);
   }
 };
 
 export async function OPTIONS() {
+  // Handle preflight requests for CORS
   return NextResponse.json(
     { message: "CORS preflight successful" },
     { status: 204, headers: addCorsHeaders() }
